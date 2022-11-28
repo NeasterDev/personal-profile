cc2c627a4a459cf7d1c53b563a036a52543289db
Nicholas Easter <nicholas.easter17@gmail.com>
2022-11-28 01:38:27 -0800

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,26 @@
+export const Nav = () => {
+  return (
+    <header>
+      <div id="nav-logo">NICHOLAS EASTER</div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <a>
+              <span className="nav-text">About</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="nav-text">Projects</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="nav-text">Contact</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
