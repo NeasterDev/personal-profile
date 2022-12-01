d799d0d67c82484997ccee03c976d8bc3d16e695
Nicholas Easter <nicholas.easter17@gmail.com>
2022-12-01 11:16:10 -0800

    added selfie picture and lorem text for description

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import selfie from "../assets/profile_pic.png";
 
 export class AboutMe extends React.Component {
     constructor(props: {}) {
         super(props);
         this.state = {};
     }
+    
+    filler = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam provident suscipit culpa earum delectus maxime deleniti consequuntur laudantium neque voluptates in, labore nemo, ratione ex eum, fugit quisquam dolor ducimus?";
 
     render() {
         return (
             <section className="about-me-container">
-                <div className="img-container">img</div>
-                <div className="about-description">description</div>
+                <div className="img-container">
+                    <img id="selfie" src={selfie} />
+                </div>
+                <div className="about-description">{this.filler}</div>
             </section>
         )
     }
