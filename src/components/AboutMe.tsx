7460979cabc58db1ff6884bdfca2b89a6466408b
Nicholas Easter <nicholas.easter17@gmail.com>
2022-12-22 20:44:44 -0800

    added cool typing effect header for the about section that types out my name, also changed the size of my selfie image so that it is bigger and the smaller screens dont look so empty, also changed the bg color of the nav bar to the transparent version so that it is still readable but doesnt look so bad.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,19 +6,7 @@ export class AboutMe extends React.Component {
 		super(props);
 		this.state = {};
 	}
-
-<<<<<<< HEAD
-    render() {
-        return (
-            <section className="about-me-container">
-                <div className="img-container">
-                    <img id="selfie" src={selfie} />
-                </div>
-                <div className="about-description">{this.filler + this.filler + this.filler}</div>
-            </section>
-        )
-    }
-=======
+	
 	filler =
 		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam provident suscipit culpa earum delectus maxime deleniti consequuntur laudantium neque voluptates in, labore nemo, ratione ex eum, fugit quisquam dolor ducimus?";
 
@@ -28,11 +16,15 @@ export class AboutMe extends React.Component {
 				<div className="img-container">
 					<img id="selfie" src={selfie} />
 				</div>
-				<div className="about-description">
-					{this.filler + this.filler + this.filler}
+				<div>
+					<div className="typewriter">
+						<h1>Nicholas Easter</h1>
+					</div>
+					<div className="about-description">
+						{this.filler + this.filler + this.filler}
+					</div>
 				</div>
 			</section>
 		);
 	}
->>>>>>> 2ed6890edde58392d6b9adad9fd2b616a9091b59
 }
